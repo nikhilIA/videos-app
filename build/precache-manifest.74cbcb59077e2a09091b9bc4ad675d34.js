1cb6425051ba9bc66ea9461f09ff84647a26d3f0
nikhilIA <nikhil.interactive@gmail.com>
2020-01-23 12:09:27 +0530

    second-build-published
    second-build-published

---
+++ build/precache-manifest.74cbcb59077e2a09091b9bc4ad675d34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a27fd11675116661a3f5ae16f0933e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e58b6eaf59d289bb3ea",
+    "url": "/static/css/main.2bef89c8.chunk.css"
+  },
+  {
+    "revision": "2e4a8bacfa16cd6ab4fd",
+    "url": "/static/js/2.b9c54d00.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b9c54d00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e58b6eaf59d289bb3ea",
+    "url": "/static/js/main.e13ac964.chunk.js"
+  },
+  {
+    "revision": "b349636d7ec7319b13f4",
+    "url": "/static/js/runtime-main.5132ebb7.js"
+  }
+]);
